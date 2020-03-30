8d41c1710d63129374b218598bef2c1044b31b63
sunnyhada <qkrtjsgml423@korea.ac.kr>
2020-03-30 11:39:27 +0900

    Goldbach_edited

--- Goldbach.c
+++ Goldbach.c
@@ -1,88 +1,64 @@
 #include <stdio.h>
+#include <math.h>
 
-int makePrime(int num, int *Prime, int *numofPrime)
+void isPrime(int *Prime, int size)
 {
-	int i = 1;
-	int j = 2;
-	
-	int cnt = 0;
-	
-	while( i < num)
+	int i = 2;
+	int j;
+
+	/* 에라토스 테네스의 체를 이용하여 소수판별
+	   소수는 그대로 0, 소수가 아닌것은 1으로 */
+
+	while( i < (int)sqrt(size) ) //총 배열의 크기의 루트값 만큼만 반복 시행
 	{
-		j = 2;
-		while(j <= i)
+		j = i*i;
+		while( j <= size )
 		{
-			if(i%j==0) break;
-			j++;
-		}
-		if(i == j)
-		{	
-			Prime[cnt] = i;
-			cnt ++;
+			if(Prime[j] % i == 0) Prime[j] = 0;
+			i + j;
 		}
 		i++;
 	}
 
-	*numofPrime = cnt;
-
-	return 0;
-}	
-
-int PrimeCnt(int *Prime, int numofPrime, int num)
-{
-	int cnt = 0;
-	
-	while (Prime[cnt] <= num && cnt < numofPrime) cnt++;
-
-	return cnt;
 }
 
-int Goldbach(int *Prime, int numofPrime, int num, int *a, int *b)
+void GoldBach(int *Prime, int num)
 {
-	int cnt = PrimeCnt(Prime, numofPrime, num);
-	int i = cnt/2;
-	int j = cnt/2;
+	int i = num/2;
+	/* 골드바흐는 2개의 수를 합한것이므로 num/2번째 부터 탐색 시작.
+	   최소차를 출력하라 했으므로 num/2 부터 점차 배열의 처음으로 가면서 탐색. */
 
-	while(i < cnt)
+	while ( i > 0 )
 	{
-		j = cnt/2;
-		while(j>=0)
+		if( Prime[i] != 1 && Prime[num - i] != 1 )
 		{
-			if(Prime[i] + Prime[j] == num)
-			{
-				*a = j;
-				*b = i;
-				return 0;
-			}
-			j--;
+			/*배열 자체가 모든 수를 가지고 있으므로 자기자신 = 배열의 순서가 됨*/
+			fprintf(stdout, "%d %d\n", i, num-i); 
+			break;
 		}
-		i++;
 	}
-
-	return 0;
 }
 
 int main(void)
 {
-	int T;
+	/*총 수의 크기만큼을 배열로 만들기*/
+	int Prime[100000] = { 0, }; 
+	int T, num;
 	int i = 0;
-	int Prime[1000];
 
-	int num;
-	int numofPrime;
-	int a, b;
+	isPrime(Prime, 100000);
 
 	fscanf(stdin, "%d", &T);
 
-	makePrime(1000, Prime, &numofPrime);
-
-	while(i<T)
+	while( i < T )
 	{
 		fscanf(stdin, "%d", &num);
-		Goldbach(Prime, numofPrime, num, &a, &b);
-		fprintf(stdout, "%d %d\n", Prime[a], Prime[b]);
+		GoldBach(Prime, num);
 		i++;
 	}
 
 	return 0;
 }
+	
+
+	
