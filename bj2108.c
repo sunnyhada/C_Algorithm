f1e352fe5edc13092f539e223076ba837522f866
sunnyhada <qkrtjsgml423@korea.ac.kr>
2020-04-08 19:19:40 +0900

    Selection -> Quick Sort

--- bj2108.c
+++ bj2108.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 
-void SelectionSort(int *arr, int len)
+void QuickSort(int *arr, int start, int end)
+{
+	if(start>=end)
+	{
+		return;
+	}
+
+	int pivot = start;
+	int i = pivot+1;
+	int j = end;
+	int temp;
+
+	while(i <= j){
+		while(i<=end && arr[i] <= arr[pivot]) i++;
+		while(j>start && arr[j] >= arr[pivot]) j--;
+
+		if(i>j)
+		{
+			temp = arr[j];
+			arr[j] = arr[pivot];
+			arr[pivot] = temp;
+		}
+
+		else
+		{
+			temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+	}
+
+	QuickSort(arr, start, j-1);
+	QuickSort(arr, j+1, end);
+}
+
+
+
+/*void SelectionSort(int *arr, int len)
 {
 	int i = 0;
 	int j = 0;
@@ -27,8 +64,11 @@ void SelectionSort(int *arr, int len)
 		i++;
 	}
 }
+*/
+
 
 //평균
+/*
 double Mean(int *arr, int len)
 {
 	double result = 0;
@@ -43,6 +83,7 @@ double Mean(int *arr, int len)
 
 	return result;
 }
+*/
 
 //중앙값
 int Median(int *arr, int len)
@@ -73,6 +114,7 @@ int Mode(int *arr, int len)
 		Mode[i] = 0;
 		i++;
 	}
+	
 
 	//빈도수를 세서 저장
 	i = 0;
@@ -135,12 +177,15 @@ int main(void)
 	while(i<N)
 	{
 		fscanf(stdin, "%d", &arr[i]);
+		mean += arr[i];
 		i++;
 	}
 
-	SelectionSort(arr, N);
+	//SelectionSort(arr, N);
 
-	mean = Mean(arr,N);
+	QuickSort(arr, 0, N);
+
+	mean = mean/N;
 	median = Median(arr,N );
 	range = Range(arr, N);
 	mode = Mode(arr, N);
