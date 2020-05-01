588d38db5db0043b423cb244a34dd1ad06bfee33
sunnyhada <qkrtjsgml423@korea.ac.kr>
2020-05-01 14:54:53 +0900

    star22 | bj10997.c hurricane star

---
+++ bj10997.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+
+void Star(int len)
+{
+	for(int i = 0; i<len+2; i++)
+	{
+		if(i==0 || i == len+1) //맨 위&아래
+			for(int j =0; j<len; j++) printf("*");
+			
+		else
+		{
+			if(i%2==1)
+			{
+				if(i<(len+1)/2)
+				{
+					for(int j=0; j<(i+1)/2; j++)
+					{
+						printf("*");
+						printf(" ");
+					}
+					for(int j=0; j<len-4*(i/2)-2;j++)
+					{
+						printf(" ");
+					}
+					for(int j=0; j<i/2; j++)
+					{
+						printf(" ");
+						printf("*");
+					}
+				}
+
+				else if(i==len/2+1)
+				{
+					for(int j=0; j<(i+1)/2-1; j++)
+					{
+						printf("*");
+						printf(" ");
+					}
+					printf("*");
+					for(int j=0; j<i/2; j++)
+					{
+						printf(" ");
+						printf("*");
+					}
+				}
+
+				else
+				{
+					for(int j=0; j<(len-i+1)/2+1; j++)
+					{
+						printf("*");
+						printf(" ");
+					}
+					for(int j=0; j<(len-4*((len-i)/2+1));j++)
+					{
+						printf(" ");
+					}
+					for(int j=0; j<(len-i)/2+1; j++)
+					{
+						printf(" ");
+						printf("*");
+					}
+				}
+			}
+			else
+			{
+				if(i<(len+1)/2)
+				{
+					for(int j=0; j<(i+1)/2; j++)
+					{
+						printf("*");
+						printf(" ");
+					}
+					for(int j=0; j<len-4*(i/2)+2;j++)
+					{
+						printf("*");
+					}
+					for(int j=0; j<i/2-1; j++)
+					{
+						printf(" ");
+						printf("*");
+					}
+				}
+				else
+				{
+					for(int j=0; j<(len-i)/2+1; j++)
+					{
+						printf("*");
+						printf(" ");
+					}
+					for(int j=0; j<(len-4*((len-i-1)/2)-4);j++)
+					{
+						printf("*");
+					}
+					for(int j=0; j<(len-i)/2+1; j++)
+					{
+						printf(" ");
+						printf("*");
+					}
+				}
+			}
+		}
+		printf("\n");
+	}
+}
+
+int main(void)
+{
+	int num=0;
+	scanf("%d", &num);
+
+	int len = num*4-3;
+	Star(len);
+	return 0;
+}
